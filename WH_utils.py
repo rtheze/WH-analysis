ad96f02edc75705d2393fb7ebd0e6fb550c00f90
brainhacker <brainhacker@NIDS4-vm-11.BrainHack>
2020-03-11 16:28:18 +0100

    bla bla

--- WH_utils.py
+++ WH_utils.py
@@ -16,15 +16,17 @@ def get_WH_data_for_given_year(data_path: str, year: int) -> pd.DataFrame:
     '''
 
 
+def get_WH_data_for_given_year(data_file: str, year: int) -> pd.DataFrame:
+    
     # Extract data
     WH_df = pd.read_csv(data_file)
     
     # Select year
-    selected_rows = WH_df.loc[WH_df['Year']==2018]
+    selected_rows = WH_df.loc[WH_df['Year']==year]
     
     # Select Category
     selected_columns = WH_df.columns[2:9]
-    
     # Output 
-    WH_year_df = WH_df.loc(selected_rows, selected_columns)
+    # WH_year_df = WH_df.loc[selected_rows, selected_columns]
+    WH_year_df = selected_rows[selected_columns]
     return WH_year_df
